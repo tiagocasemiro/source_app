6632c23472a877f5b42784c495ae3862708b6dd3
Tiago <tiagocasemiro@hotmail.com>
2020-08-03 09:24:27 -0300

    Add restore adapter

---
+++ lib/engine/shell/git/adapter/restore_adapter.dart
@@ -0,0 +1,7 @@
+
+import 'package:source_app/engine/shell/git/adapter/base/base_adapter.dart';
+import 'package:source_app/engine/shell/model/terminal_output.dart';
+
+class RestoreAdapter extends BaseAdapter {
+  RestoreAdapter(TerminalOutput terminalOutput) : super(terminalOutput);
+}
