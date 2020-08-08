8e5c53ce0eb471a40b8dde59a73d2d011a290e1e
Tiago <tiagocasemiro@hotmail.com>
2020-08-08 12:47:38 -0300

    Add state to repository local

--- lib/engine/domain/model/git_repository.dart
+++ lib/engine/domain/model/git_repository.dart
@@ -2,11 +2,19 @@
 class Repository {
   String name;
   String workDirectory;
+  State state;
 
   Repository(this.name, this.workDirectory);
 
   @override
   String toString() {
-    return 'Repository{name: $name, workDirectory: $workDirectory}';
+    return 'Repository{name: $name, workDirectory: $workDirectory, state: $state}';
   }
 }
+
+enum State {
+  allCommitted,
+  toCommit,
+  needUpdate
+}
+
