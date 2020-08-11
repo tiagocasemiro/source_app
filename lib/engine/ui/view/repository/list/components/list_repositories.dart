fae5d3829210c98adcf3d193322618818533b1fd
Tiago <tiagocasemiro@hotmail.com>
2020-08-11 10:09:34 -0300

    change color of card

--- lib/engine/ui/view/repository/list/components/list_repositories.dart
+++ lib/engine/ui/view/repository/list/components/list_repositories.dart
@@ -9,7 +9,7 @@ class ListRepositories extends StatelessWidget {
     return ListView.builder(
       itemBuilder: (context, index) {
         return  Container(
-          color: Colors.white12,
+          color: Colors.white10,
           width: double.maxFinite,
           height: 100,
           margin: const EdgeInsets.all(8.0),
