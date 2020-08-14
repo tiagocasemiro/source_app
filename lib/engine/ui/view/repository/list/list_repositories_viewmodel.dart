0eeded983303cad6ebda63c6921fc0ac20219f66
Tiago <tiagocasemiro@hotmail.com>
2020-08-14 10:23:27 -0300

    Add stream

--- lib/engine/ui/view/repository/list/list_repositories_viewmodel.dart
+++ lib/engine/ui/view/repository/list/list_repositories_viewmodel.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:source_app/engine/domain/model/git_repository.dart';
 import 'package:source_app/engine/domain/use.case/repository_usecase.dart';
 
 class SelectRepositoryViewModel {
+  // ignore: close_sinks
+  final StreamController<String> _streamController = StreamController();
+  Sink<String> get input => _streamController.sink;
+  Stream<String> get output => _streamController.stream;
 
   Future<List<Repository>> all() async {
     return Future.delayed(const Duration(milliseconds: 100), () {
@@ -9,12 +15,10 @@ class SelectRepositoryViewModel {
     });
   }
 
-  Future<String> status(Repository repository) async {
-    String output =  await RepositoryUseCase().statusOfRepository(repository);
-
-    print(output);
-
-    return output;
+   void status(Repository repository) {
+    RepositoryUseCase().statusOfRepository(repository).then((String content) {
+      input.add(content);
+    });
   }
 
   Future newRepository() async {
