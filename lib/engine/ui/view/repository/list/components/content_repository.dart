c99b0f5cdd5e29651c50b4f47efc1026a5fd92ba
Tiago <tiagocasemiro@hotmail.com>
2020-08-15 08:38:10 -0300

    implement delete click

--- lib/engine/ui/view/repository/list/components/content_repository.dart
+++ lib/engine/ui/view/repository/list/components/content_repository.dart
@@ -1,36 +1,46 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:source_app/engine/ui/view/repository/list/list_repositories_viewmodel.dart';
-
-import '../../../../source_resources.dart';
+import 'package:source_app/engine/domain/model/git_repository.dart';
+import 'package:source_app/engine/ui/source_resources.dart';
 
 class RepositoryDetails extends StatelessWidget {
-  final SelectRepositoryViewModel _viewModel;
-  final String _content;
+  final Repository _repository;
 
-  RepositoryDetails(this._viewModel, this._content);
+  RepositoryDetails(this._repository);
 
   @override
   Widget build(BuildContext context) {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Container(
+            width: double.maxFinite,
+            padding: const EdgeInsets.only(left: 16, top: 8, right: 8, bottom: 8),
+            decoration: BoxDecoration(
+                color: SourceColors.grey[2],
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Text(_repository.name,
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.w500,
+                color: SourceColors.blue[2],
+                fontSize: 23.0,
+              ),
+            ),
+          ),
           SingleChildScrollView(
-
-            padding: EdgeInsets.all(8.0),
+            padding: EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("\$ git status\n",
+                Text("\n\$ git status",
                   style: GoogleFonts.robotoMono(
                     fontWeight: FontWeight.w700,
                     color: SourceColors.blue[2],
-                    fontSize: 25.0,
+                    fontSize: 20.0,
                   ),
                 ),
-                Text(_content,
+                Text(_repository.status,
                   style: GoogleFonts.robotoMono(
                     fontWeight: FontWeight.w400,
                     color: SourceColors.blue[2],
