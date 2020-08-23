432342496b9848befcdf096f6bcaa37376fc365c
Tiago <tiagocasemiro@hotmail.com>
2020-08-23 16:51:54 -0300

    implement footer container

--- lib/engine/ui/view/repository/dashboard/dashboard_view.dart
+++ lib/engine/ui/view/repository/dashboard/dashboard_view.dart
@@ -3,6 +3,7 @@ import 'package:source_app/engine/ui/source_resources.dart';
 import 'package:source_app/engine/ui/utils/default_values.dart';
 import 'package:source_app/engine/ui/view/repository/dashboard/components/body_left_dashboard.dart';
 import 'package:source_app/engine/ui/view/repository/dashboard/components/body_right_dashboard.dart';
+import 'package:source_app/engine/ui/view/repository/dashboard/components/footer_dashboard.dart';
 import 'package:source_app/engine/ui/view/repository/dashboard/components/footer_left_dashboard.dart';
 import 'package:source_app/engine/ui/view/repository/dashboard/components/footer_right_dashboard.dart';
 import 'package:source_app/engine/ui/view/repository/dashboard/components/header_dashboard.dart';
@@ -53,14 +54,16 @@ class Dashboard extends StatelessWidget {
                     child: BodyRightDashboard(),
                   ),
                   down: Container(
-                    child: VerticalSplitView(
-                      left: Container(
-                        padding: const EdgeInsets.only(bottom: defaultPaddingSize),
-                        child: FooterLeftDashboard(),
-                      ),
-                      right: Container(
-                        padding: const EdgeInsets.only(right: defaultPaddingSize, bottom: defaultPaddingSize),
-                        child: FooterRightDashboard(),
+                    child: FooterDashboard(
+                      VerticalSplitView(
+                        left: Container(
+                          padding: const EdgeInsets.only(bottom: defaultPaddingSize),
+                          child: FooterLeftDashboard(),
+                        ),
+                        right: Container(
+                          padding: const EdgeInsets.only(right: defaultPaddingSize, bottom: defaultPaddingSize),
+                          child: FooterRightDashboard(),
+                        ),
                       ),
                     ),
                   ),
