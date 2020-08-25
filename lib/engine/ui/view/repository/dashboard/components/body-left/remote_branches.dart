6c07a0407c39fe9d90edd713df55e1441f175eb1
Tiago <tiagocasemiro@hotmail.com>
2020-08-25 09:56:02 -0300

    Add remote commit widget

---
+++ lib/engine/ui/view/repository/dashboard/components/body-left/remote_branches.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:source_app/engine/ui/source_resources.dart';
+
+class RemoteBranches extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+          color: SourceColors.grey[2],
+          borderRadius: BorderRadius.circular(10)
+      ),
+    );
+  }
+}
