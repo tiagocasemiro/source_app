0f7e0e1a0a46b2f21371ce2ab5647fdaafc12d36
Tiago <tiagocasemiro@hotmail.com>
2020-08-27 20:59:14 -0300

    Git command  with credentials

--- lib/engine/shell/terminal.dart
+++ lib/engine/shell/terminal.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import 'package:source_app/engine/shell/git/git.dart';
 import 'package:source_app/engine/shell/model/terminal_output.dart';
 
 class Terminal {
@@ -18,9 +20,34 @@ class Terminal {
     });
   }
 
-  void _printOutput(processResult) {
-    print("Error output : " + processResult.stderr.toString());
-    print("Saida output : " + processResult.stdout.toString());
-    print("Exit code : " + processResult.exitCode.toString());
+  Future<TerminalOutput> runWithGitCredentials(String command, {List<String> parameters = const [], Map<String, String> environment}) async {
+    Process process = await Process.start(command, parameters, workingDirectory: _workDirectory, environment: environment);
+    var completer = new Completer<TerminalOutput>();
+    process.stdout.transform(utf8.decoder).listen((data) {
+      if(data.toLowerCase().contains("username")) {
+        process.stdin.writeln(Git.username);
+      } else if(data.toLowerCase().contains("password")) {
+        process.stdin.writeln(Git.password);
+      } else {
+        _printOutput(data, exitCode.toString(), command, parameters: parameters);
+        completer.complete(TerminalOutput(data, 0));
+      }
+    });
+    process.stderr.transform(utf8.decoder).listen((data) {
+      _printOutput( data.toString(), exitCode.toString(), command, parameters: parameters);
+      completer.complete(TerminalOutput(data, 1));
+    });
+
+    return completer.future;
+  }
+
+  void _printOutput(String message, String exitCode, String command, {List<String> parameters = const []}) {
+    parameters.forEach((element) {
+      command += " $element";
+    });
+    print("Git command: " + command);
+    print("Directory: " + _workDirectory);
+    print("Message : " + message);
+    print("Exit code : " + exitCode);
   }
 }
