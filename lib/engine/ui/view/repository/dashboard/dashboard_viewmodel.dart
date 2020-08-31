101bd9811dfcba345f4acc454d7c37f16f60d6cb
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-08-31 10:37:47 -0300

    Modify view model to save credentials

--- lib/engine/ui/view/repository/dashboard/dashboard_viewmodel.dart
+++ lib/engine/ui/view/repository/dashboard/dashboard_viewmodel.dart
@@ -1,15 +1,25 @@
-import 'dart:async';
 import 'package:source_app/engine/domain/model/git_repository.dart';
 import 'package:source_app/engine/domain/use.case/start_application_usecase.dart';
 import 'package:source_app/engine/shell/git/git.dart';
 import 'package:source_app/engine/shell/git/model/git_output.dart';
+import 'dart:async';
 
 class DashboardViewModel {
   final StreamController<GitOutput> _localBranchesController = StreamController<GitOutput>.broadcast();
   Stream<GitOutput> get localBranchesOutput => _localBranchesController.stream;
 
-  Future<bool> initRepository(Repository repository, String username, String password) async {
-    return await StartApplicationUseCase().startGitApplication(repository.workDirectory, username, password);
+  Future<bool> checkCredentialAndInitRepository(Repository repository, String username, String password) async {
+    StartApplicationUseCase startApplicationUseCase = StartApplicationUseCase();
+    bool isStarted = await startApplicationUseCase.startGitApplication(repository.workDirectory, username, password);
+    if(isStarted) {
+      return await startApplicationUseCase.checkCredentials(repository.workDirectory);
+    }
+
+    return false;
+  }
+
+  Future<bool> checkCredentials(Repository repository) async {
+    return await StartApplicationUseCase().checkCredentials(repository.workDirectory);
   }
 
   void allLocalBranches() async {
