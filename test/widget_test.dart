d19cfb1567a48bf9ae8fa747d175c2ee2afe4413
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-12 18:46:02 -0300

    rename package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:source_app/engine/ui/view/repository/list/list_repositories_view.dart';
+import 'package:source_app/engine/ui/screen/repository/list/list_repositories_view.dart';
 
 import 'package:source_app/main.dart';
 
