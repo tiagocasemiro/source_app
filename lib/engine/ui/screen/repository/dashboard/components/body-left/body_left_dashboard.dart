0240aa5e283123aeaa2145a833fee30098cd7f6a
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-14 17:14:49 -0300

    pass viewmodel

--- lib/engine/ui/screen/repository/dashboard/components/body-left/body_left_dashboard.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-left/body_left_dashboard.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-left/local_branches.dart';
+import 'package:source_app/engine/ui/screen/repository/dashboard/dashboard_viewmodel.dart';
 
 
 
 class BodyLeftDashboard extends StatelessWidget {
+
+  final DashboardViewModel _dashboardViewModel;
+
+  BodyLeftDashboard(this._dashboardViewModel);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          LocalBranches(),
+          LocalBranches(_dashboardViewModel),
+
         ],
       ),
     );
