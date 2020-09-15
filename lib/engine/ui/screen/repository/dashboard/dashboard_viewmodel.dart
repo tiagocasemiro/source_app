b4494ce3168f16f3b18f2cc7975a60ea24952b81
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-15 19:09:28 -0300

    create new local branch method

--- lib/engine/ui/screen/repository/dashboard/dashboard_viewmodel.dart
+++ lib/engine/ui/screen/repository/dashboard/dashboard_viewmodel.dart
@@ -1,4 +1,6 @@
+import 'package:source_app/engine/domain/model/git_branch.dart';
 import 'package:source_app/engine/domain/model/git_repository.dart';
+import 'package:source_app/engine/domain/use.case/branches_usecase.dart';
 import 'package:source_app/engine/domain/use.case/start_application_usecase.dart';
 import 'package:source_app/engine/shell/git/git.dart';
 import 'package:source_app/engine/shell/git/model/git_output.dart';
@@ -10,7 +12,7 @@ class DashboardViewModel {
 
   Future<bool> checkCredentialAndInitRepository(Repository repository, String username, String password) async {
     StartApplicationUseCase startApplicationUseCase = StartApplicationUseCase();
-    bool isStarted = await startApplicationUseCase.startGitApplication(repository.workDirectory, username, password);
+    bool isStarted = await startApplicationUseCase.startGitApplicationWithCredentials(repository.workDirectory, username, password);
     if(isStarted) {
       return await startApplicationUseCase.checkCredentials(repository.workDirectory);
     }
@@ -25,6 +27,15 @@ class DashboardViewModel {
   void allLocalBranches() async {
     GitOutput gitOutput = await  Git().branch().local().call();
     _localBranchesController.sink.add(gitOutput);
+  }
+
+  Future<List<GitBranch>> localBranches() async {
+    GitOutput gitOutput = await BranchesUseCase().local();
+    if(gitOutput.isSuccess()) {
+      return gitOutput.object as List<GitBranch>;
+    }
+
+    return null;
   }
 
   void dispose() {
