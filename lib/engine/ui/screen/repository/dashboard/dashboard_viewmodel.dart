0c6a81a1f6144e2fc7d74f28827f9e7627954345
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-16 15:15:41 -0300

    Add reload to local branches

--- lib/engine/ui/screen/repository/dashboard/dashboard_viewmodel.dart
+++ lib/engine/ui/screen/repository/dashboard/dashboard_viewmodel.dart
@@ -2,13 +2,12 @@ import 'package:source_app/engine/domain/model/git_branch.dart';
 import 'package:source_app/engine/domain/model/git_repository.dart';
 import 'package:source_app/engine/domain/use.case/branches_usecase.dart';
 import 'package:source_app/engine/domain/use.case/start_application_usecase.dart';
-import 'package:source_app/engine/shell/git/git.dart';
 import 'package:source_app/engine/shell/git/model/git_output.dart';
 import 'dart:async';
 
 class DashboardViewModel {
-  final StreamController<GitOutput> _localBranchesController = StreamController<GitOutput>.broadcast();
-  Stream<GitOutput> get localBranchesOutput => _localBranchesController.stream;
+  final StreamController<bool> _localBranchesController = StreamController<bool>.broadcast();
+  Stream<bool> get localBranchesOutput => _localBranchesController.stream;
 
   Future<bool> checkCredentialAndInitRepository(Repository repository, String username, String password) async {
     StartApplicationUseCase startApplicationUseCase = StartApplicationUseCase();
@@ -24,9 +23,8 @@ class DashboardViewModel {
     return await StartApplicationUseCase().checkCredentials(repository.workDirectory);
   }
 
-  void allLocalBranches() async {
-    GitOutput gitOutput = await  Git().branch().local().call();
-    _localBranchesController.sink.add(gitOutput);
+  void refreshLocalBranches() async {
+    _localBranchesController.sink.add(true);
   }
 
   Future<List<GitBranch>> localBranches() async {
