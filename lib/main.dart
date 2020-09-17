9740643197b2a2bc8c7ee54096d9c4f836c8ca54
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-17 10:16:35 -0300

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:source_app/sample_test.dart';
-import 'engine/domain/model/git_repository.dart';
+import 'dart:io';
 
-import 'engine/domain/use.case/repository_usecase.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'engine/ui/source_resources.dart';
-import 'engine/ui/view/repository/list/list_repositories_view.dart';
+import 'engine/ui/screen/repository/list/list_repositories_view.dart';
+
+import 'dart:io';
+import 'dart:convert';
+
+void shell() async {
+  const Map<String, String> _language = {'LANG':'en_GB'};
+  var process = await Process.start('git', ["remote", "show", "origin",], workingDirectory: "/directory/repo"
+  ,environment: _language);
+  process.stdout.transform(utf8.decoder).listen((data) {
+    if(data.contains("password")) {
+      process.stdin.write("123456");
+    }
+    print(data);
+  });
+  process.stderr.transform(utf8.decoder).listen((data) {
+    print(data);
+  });
+  process.exitCode.then((exitCode) {
+    print(exitCode);
+  });
+}
 
 
 void main() {
   runApp(MainWidget(SelectRepositoryView()));
-
+  //runApp(MainWidget(MyApp()));
+  //runApp(new MaterialApp(home: new MyApp(), debugShowCheckedModeBanner: false,),);
   //runApp(AnimatedListSample());
 
 //https://medium.com/@leonar.d/how-to-create-a-flutter-split-view-7e2ac700ea12
@@ -22,32 +44,26 @@ void main() {
   //Git().isGitDirectory().then((branches) => print(branches));
   //Git().log().graph().pretty(Log.historyFormat).call().then((branches) => print(branches));
   //Git().restore().files(['cument_5.txt']).call();
-  //StartRepositoryUseCase().startGitApplication(workDirectory, username, password).then((value) => print(value));
 
- // String workDirectory = "/home/tiagocasemiro/Documentos/projetos/pessoal/documentation";
-  //String workDirectorySalesman = "/home/tiagocasemiro/Documentos/projetos/ayla/ayla-package-application-salesman";
 
- // RepositoryUseCase().addLocalRepository(Repository("Salesman", workDirectorySalesman)).then((value) => print(value)); // SQLite dont work on desktop
-
-/*  RepositoryUseCase().allLocalRepository().then((List<Repository> repositories) {
-    repositories.forEach((element) {
-      print(element);
-    });
-  });*/
-
-  //var repository = Repository("Documentation", workDirectory);
-
-  //RepositoryUseCase().deleteLocalRepository(repository).then((value) => print(value));
+  //gitclone
+  //https://www.atlassian.com/br/git/tutorials/setting-up-a-repository/git-clone
 
   //Alternatives
-
-  // NoSql key value storage
-  //https://github.com/tekartik/sembast.dart/blob/master/sembast/doc/open.md
-  //https://pub.dev/packages/sembast
-  //https://pub.dev/packages/sembast/example
+  //Seguimented control
+  //https://medium.com/flutterpub/segmentedcontrol-in-flutter-643685290adb
 
   // File
   //https://flutter.dev/docs/cookbook/persistence/reading-writing-files
+  //https://pub.dev/packages/material_segmented_control
+
+  //spannable listview
+  //https://stackoverflow.com/questions/50530152/how-to-create-expandable-listview-in-flutter
+  //https://medium.com/flutter-community/flutter-expansion-collapse-view-fde9c51ac438
+
+  //Flex with listview
+  //https://www.youtube.com/watch?v=_rnZaagadyo
+  //https://www.youtube.com/watch?v=z5iw2SeFx2M
 }
 
 class MainWidget extends StatelessWidget {
@@ -59,6 +75,7 @@ class MainWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Source',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: SourceColors.grey[4],
           accentColor: SourceColors.blue,
@@ -69,3 +86,67 @@ class MainWidget extends StatelessWidget {
     );
   }
 }
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Vehicle veicle =new Vehicle(
+      'Branch local',
+      ['RECHARGE-024', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099', 'RECHARGE-084', 'RECHARGE-075', 'RECHARGE-099'],
+      Icons.call_merge,
+    );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ExpansionTile(
+        title:  new ListTile(
+          contentPadding: const EdgeInsets.all(4),
+          title: Text(veicle.title, style: GoogleFonts.roboto(
+            fontWeight: FontWeight.w600,
+            color: SourceColors.blue[2],
+            fontSize: 12.0,
+          ),),
+          leading: Image.asset("images/branch-local.png", width: 10, height: 10, fit: BoxFit.scaleDown,),
+        ),
+
+        children: <Widget>[
+          new Column(
+            children: _buildExpandableContent(veicle),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildExpandableContent(Vehicle vehicle) {
+    List<Widget> columnContent = [];
+
+    for (String content in vehicle.contents)
+      columnContent.add(
+        new ListTile(
+          contentPadding: const EdgeInsets.all(4),
+          title: Text(content, style: new TextStyle(fontSize: 18.0),),
+          leading: Padding(child: Image.asset("images/branch-local.png"), padding: const EdgeInsets.all(16),),
+        ),
+      );
+
+    return columnContent;
+  }
+}
+
+class Vehicle {
+  final String title;
+  List<String> contents = [];
+  final IconData icon;
+
+  Vehicle(this.title, this.contents, this.icon);
+}
+
+
