36384322fdf80c01279caf576128daaf34ead03e
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-20 15:02:36 -0300

    Ajust import

--- lib/engine/ui/screen/repository/dashboard/components/body-left/remote_branches.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-left/remote_branches.dart
@@ -3,9 +3,9 @@ import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:source_app/engine/domain/model/git_branch.dart';
 import 'package:source_app/engine/shell/git/model/git_output.dart';
+import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-left/body_left_viewmodel.dart';
 import 'package:source_app/engine/ui/source_resources.dart';
 
-import 'body_left_viewmodel.dart';
 
 class RemoteBranches extends StatefulWidget {
   final BodyLeftViewModel _dashboardViewModel;
