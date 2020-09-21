79411c0af1c9d33b3dd23e107c4d056bac8f8e20
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-21 15:41:54 -0300

    Alert error terminal message

---
+++ lib/engine/ui/widgets/gitoutput_error_alert.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:source_app/engine/shell/git/model/git_output.dart';
+import 'package:source_app/engine/ui/source_resources.dart';
+
+class GitOutputErrorAlert {
+  final GitOutput _gitOutput;
+
+  GitOutputErrorAlert(this._gitOutput);
+
+  displayAlert(BuildContext context) {
+    Widget closeButton = RaisedButton(
+      color: SourceColors.red,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Text("close",
+        style: GoogleFonts.roboto(
+          fontWeight: FontWeight.w300,
+          color: SourceColors.white,
+          fontSize: 18.0,
+        ),
+      ),
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+      },
+    );
+
+    AlertDialog alertDialog = AlertDialog(
+      title: Text("Terminal output error",
+        style: GoogleFonts.balooBhai(
+          fontWeight: FontWeight.w300,
+          color: SourceColors.red,
+          fontSize: 25.0,
+        ),
+      ),
+      backgroundColor: SourceColors.grey[7],
+      actionsPadding: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      content: Container(
+        width: 650,
+        child: Text(_gitOutput.message, style: GoogleFonts.roboto(
+          fontWeight: FontWeight.w400,
+          color: SourceColors.blue[2],
+          fontSize: 16.0,
+        ),),
+      ),
+      actions: [
+        SizedBox(
+          width: 150,
+          height: 40,
+          child: closeButton,
+        ),
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      },
+      barrierDismissible: false,
+    );
+  }
+}
