ab873be6dbab61b2e93ef7b11082697d695bb532
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-21 15:42:42 -0300

    add red

--- lib/engine/ui/source_resources.dart
+++ lib/engine/ui/source_resources.dart
@@ -10,11 +10,12 @@ class SourceColors {
       4: Color(0xFF000000),
       5: Color(0xFFC4C4C4),
       6: Color(0xFFF8F8F8),
+      7: Color(0xFFF1F1F1),
     },
   );
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-
+//B90000
   static const MaterialColor blue = MaterialColor(0xFF0025EA,
     <int, Color>{
       1: Color(0xFF0025EA),
@@ -24,6 +25,12 @@ class SourceColors {
       5: Color(0x140025EA),
       6: Color(0x50172B4D),
       7: Color(0xFFF3F6FE),
+    },
+  );
+
+  static const MaterialColor red = MaterialColor(0xFFB90000,
+    <int, Color>{
+      1: Color(0xFFB90000),
     },
   );
 }
