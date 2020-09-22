d74ab723e3935ef7e84b2e03e7e8d6e6e0fc17a2
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-22 10:55:38 -0300

    Success notification

---
+++ lib/engine/ui/widgets/gitoutput_success_snackbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:source_app/engine/shell/git/model/git_output.dart';
+import 'package:source_app/engine/ui/source_resources.dart';
+
+class GitOutputSuccessSnackBar {
+  BuildContext _context;
+  final String _defaultMessage =  "Command successfully executed";
+
+  GitOutputSuccessSnackBar(this._context);
+
+  void showWithMessage(GitOutput gitOutput) {
+    String message = gitOutput != null && gitOutput.message != null && gitOutput.message.isNotEmpty ? gitOutput.message.trim() : _defaultMessage;
+
+    Scaffold.of(_context).showSnackBar(
+        new SnackBar(
+          content: Text(message, style: GoogleFonts.roboto(
+            fontWeight: FontWeight.w500,
+            color: SourceColors.white,
+            fontSize: 18.0, ),
+          ),
+          backgroundColor: SourceColors.green,
+        )
+    );
+  }
+
+  void showDefaultMessage() {
+    Scaffold.of(_context).showSnackBar(
+        new SnackBar(
+          content: Text(_defaultMessage, style: GoogleFonts.roboto(
+            fontWeight: FontWeight.w500,
+            color: SourceColors.white,
+            fontSize: 18.0, ),
+          ),
+          backgroundColor: SourceColors.green,
+        )
+    );
+  }
+}
