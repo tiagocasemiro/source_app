24be4a95206f3dfeee3e677c2f705a1d84ab80ee
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-25 17:04:03 -0300

    update to support url service

--- lib/engine/domain/model/git_repository.dart
+++ lib/engine/domain/model/git_repository.dart
@@ -1,12 +1,14 @@
 
+import 'package:source_app/engine/shell/git/git.dart';
+
 class Repository {
   String name;
   String workDirectory;
   String status;
-  String url = "";
-  String credentials = "";
-  String username = "";
-  String password = "";
+  String url;
+  String credentials;
+  String username;
+  String password;
   String origin = "origin";
 
   Repository(this.name, this.workDirectory, {this.url});
@@ -31,33 +33,29 @@ class Repository {
   @override
   int get hashCode => name.hashCode ^ workDirectory.hashCode;
 
-  String hidePasswordCredentials() {
-    if(credentials != null && credentials.isNotEmpty) {
-      List<String> parts = credentials.split("//");
-      if(parts.length == 2) {
-        if(parts[1].contains("@")) {
-          List<String> identifier = parts[1].split("@");
-          return parts[0].trim() + "//" + identifier[0].split(":")[0] + "@" + identifier[1].trim();
-        } else {
-          return credentials;
-        }
-      }
-    }
-
-    return "";
-  }
-
   void generateCredentials() {
-    if(url != null && url.isNotEmpty && username != null && username.isNotEmpty && password != null && password.isNotEmpty) {
+    if(url != null && url.isNotEmpty) {
       List<String> parts = url.split("//");
       if(parts.length == 2) {
         if(parts[1].contains("@")) {
           List<String> identifier = parts[1].split("@");
-          credentials = parts[0].trim() + "//" + username + ":" + password + "@" + identifier[1].trim();
+          credentials = parts[0].trim() + "//\$" + Git.gitRepoUsername + ":\$" + Git.gitRepoPassword + "@" + identifier.last.split("/")[0].trim();
         } else {
-          credentials =  parts[0].trim() + "//" + username + ":" + password + "@" + parts[1].trim();
+          credentials =  parts[0].trim() + "//\$" + Git.gitRepoUsername + ":\$" + Git.gitRepoPassword + "@" + parts[1].split("/")[0].trim();
         }
       }
     }
+  }
+
+  String serviceUrl() {
+    if(url != null && url.isNotEmpty) {
+      List<String> parts = url.split("//");
+      if(parts.length == 2) {
+          String host = parts[1].split("/")[0];
+          return parts[0].trim() + "//" + host;
+      }
+    }
+
+    return null;
   }
 }
