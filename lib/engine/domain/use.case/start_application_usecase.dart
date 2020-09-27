8833ff8f18219881a2bd01d9ee63798074d8bc08
tiagocasemirom4u <tiagocasemiro@hotmail.com>
2020-09-27 13:22:36 -0300

    Identing

--- lib/engine/domain/use.case/start_application_usecase.dart
+++ lib/engine/domain/use.case/start_application_usecase.dart
@@ -1,8 +1,5 @@
-import 'package:source_app/engine/domain/model/git_remote.dart';
 import 'package:source_app/engine/domain/model/git_repository.dart';
-import 'package:source_app/engine/shell/git/command/remote.dart';
 import 'package:source_app/engine/shell/git/git.dart';
-import 'package:source_app/engine/shell/git/model/git_output.dart';
 
 class StartApplicationUseCase {
   Future<String> startGitApplicationWithCredentials(Repository repository) async {
