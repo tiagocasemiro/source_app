f2d109a561cad72a3b9e8cbd3f3803fab5d1be1b
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-15 10:04:53 -0300

    Add view model

--- lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_to_commit.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_to_commit.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-left/body_left_viewmodel.dart';
 
 class ItemFileToCommit extends StatelessWidget {
+
+  final BodyLeftViewModel _bodyLeftViewModel;
+
+  ItemFileToCommit(this._bodyLeftViewModel);
+
   @override
   Widget build(BuildContext context) {
     return Container();
