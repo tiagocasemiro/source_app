7338ef864d50fd6e06b671a9aa4000e814235fca
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-16 18:38:47 -0300

    add callback

--- lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_history.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_history.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ItemHistory extends StatelessWidget {
+  final Function _onClick;
+
+  ItemHistory(this._onClick(String hash));
+
   @override
   Widget build(BuildContext context) {
     return Container();
