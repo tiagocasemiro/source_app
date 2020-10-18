b2eb46609525fd4a3ba0dd20e2937fb02095d931
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-18 15:00:49 -0300

    remove import

--- lib/engine/ui/screen/repository/dashboard/components/header/components/menu_commit.dart
+++ lib/engine/ui/screen/repository/dashboard/components/header/components/menu_commit.dart
@@ -1,4 +1,3 @@
-import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-right/body_right_viewmodel.dart';
 import 'package:source_app/engine/ui/screen/repository/dashboard/components/header/header_viewmodel.dart';
 import 'package:source_app/engine/ui/screen/repository/dashboard/components/header/widget/menu_button.dart';
 import 'package:flutter/material.dart';
