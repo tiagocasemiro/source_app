a5a65e788835f07c3edac06c5736769ab8390cd5
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-18 15:02:23 -0300

    remove import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:source_app/engine/ui/screen/repository/list/list_repositories_view.dart';
 
-import 'package:source_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
