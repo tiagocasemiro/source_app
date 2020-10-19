537af6e108eb380e7cc3c06c2f71db862b0242e6
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-19 18:04:55 -0300

    rename file

--- lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_to_commit.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_to_commit.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-left/body_left_viewmodel.dart';
 
-class ItemFileToCommit extends StatelessWidget {
+class ItemFileFromCommit extends StatelessWidget {
 
   final BodyLeftViewModel _bodyLeftViewModel;
 
-  ItemFileToCommit(this._bodyLeftViewModel);
+  ItemFileFromCommit(this._bodyLeftViewModel);
 
   @override
   Widget build(BuildContext context) {
