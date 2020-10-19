79837501e446bbd735927ec89f9e0facc263a7a6
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-19 16:09:11 -0300

    add diff tree variant of command

--- lib/engine/shell/git/command/diff.dart
+++ lib/engine/shell/git/command/diff.dart
@@ -4,6 +4,7 @@ import 'package:source_app/engine/shell/git/model/git_output.dart';
 import 'package:source_app/engine/shell/model/terminal_output.dart';
 
 class Diff extends BaseGitCommand {
+  _Variant _variant = _Variant.diff;
   Diff(String workDirectory) : super(workDirectory) {
     parameters.add('diff');
   }
@@ -26,10 +27,41 @@ class Diff extends BaseGitCommand {
     return this;
   }
 
+  Diff tree() {
+    parameters.clear();
+    parameters.add("diff-tree");
+    _variant = _Variant.diff_tree;
+
+    return this;
+  }
+
+  Diff noCommitId() {
+    parameters.add("--no-commit-id");
+
+    return this;
+  }
+
+  Diff r() {
+    parameters.add("-r");
+
+    return this;
+  }
+
+  Diff hash(String value) {
+    parameters.add(value);
+
+    return this;
+  }
+
   @override
   Future<GitOutput> call() async {
     TerminalOutput terminalOutput = await super.execute(parameters: parameters);
 
-    return DiffAdapter(terminalOutput).files();
+    if(_variant == _Variant.diff)
+      return DiffAdapter(terminalOutput).files();
+
+    return DiffAdapter(terminalOutput).filesModifiedFromCommit();
   }
 }
+
+enum _Variant { diff, diff_tree }
