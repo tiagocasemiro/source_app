6d6b17a3285970e395632bc3af55c6bc21493c1a
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-19 16:07:28 -0300

    prepare commit files area

--- lib/engine/ui/screen/repository/dashboard/components/body-right/components/commit_files.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/components/commit_files.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:source_app/engine/shell/git/model/git_output.dart';
 import 'package:source_app/engine/ui/source_resources.dart';
 import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-right/body_right_viewmodel.dart';
 
@@ -12,9 +13,22 @@ class CommitFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: SourceColors.grey[2], borderRadius: BorderRadius.circular(10)),
+    return StreamBuilder(
+      stream: _bodyRightViewModel.filesModifiedFromCommitOutput,
+      builder: (context, snapshot) {
+
+        if(snapshot.data is GitOutput) {
+          GitOutput gitOutput = snapshot.data as GitOutput;
+          if(gitOutput.isSuccess()) {
+            print(gitOutput.message);
+          }
+        }
+
+        return Container(
+          decoration: BoxDecoration(
+              color: SourceColors.grey[2], borderRadius: BorderRadius.circular(10)),
+        );
+      },
     );
   }
 }
