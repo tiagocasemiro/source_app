6a86eefcde26862a5c66ed1ed05bfb30fdcef879
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-20 09:20:43 -0300

    prepare stream builder

--- lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_from_commit.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_file_from_commit.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-left/body_left_viewmodel.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:source_app/engine/domain/model/git_file_modified.dart';
+import 'package:source_app/engine/shell/git/model/git_output.dart';
+import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-right/body_right_viewmodel.dart';
+import 'package:source_app/engine/ui/source_resources.dart';
 
 class ItemFileFromCommit extends StatelessWidget {
 
-  final BodyLeftViewModel _bodyLeftViewModel;
+  final BodyRightViewModel _bodyRightViewModel;
 
-  ItemFileFromCommit(this._bodyLeftViewModel);
+  ItemFileFromCommit(this._bodyRightViewModel);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+        stream: _bodyRightViewModel.filesModifiedFromCommitOutput,
+        builder: (context, snapshot) {
+          GitOutput gitOutput = snapshot.data;
+          List<GitFileModified> filesCommitted = snapshot.data is GitOutput &&
+              gitOutput.isSuccess() && gitOutput.object != null &&
+              gitOutput.object is List<GitFileModified> ? gitOutput.object: List<GitFileModified>();
+
+          return Visibility(
+              visible: filesCommitted.isNotEmpty,
+              replacement: Center(child: Text("No files on commit",
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w300,
+                  color: SourceColors.blue[2],
+                  fontSize: 16.0,
+                ),
+              )),
+              child: Container()
+          );
+        }
+    );
   }
 }
