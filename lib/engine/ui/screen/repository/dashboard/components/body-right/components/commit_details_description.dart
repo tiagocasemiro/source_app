a3004da5d59ce9dced5efeca40e9de3dbe8845a6
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-10-21 10:06:32 -0300

    Add description file

---
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/components/commit_details_description.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:source_app/engine/ui/screen/repository/dashboard/components/body-right/body_right_viewmodel.dart';
+
+class CommitDetailsDescription extends StatelessWidget {
+  final BodyRightViewModel _bodyRightViewModel;
+
+  CommitDetailsDescription(this._bodyRightViewModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
