5e073d2a1d3c04e6adead954eadb9db255e70517
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-11-04 10:14:08 -0300

    Add suport to graph

--- lib/engine/domain/model/git_commit.dart
+++ lib/engine/domain/model/git_commit.dart
@@ -6,12 +6,26 @@ class GitCommit {
   String date;
   String author;
   String beforeHash;
-  List<String> asciiGraphs;
+  GraphHelper graphHelper;
 
-  GitCommit({this.asciiGraph = "", this.abbreviatedHash = "", this.author = "", this.message = "", this.date = "", this.hash = "", this.beforeHash = "", this.asciiGraphs});
+  GitCommit({this.asciiGraph = "", this.abbreviatedHash = "", this.author = "", this.message = "", this.date = "", this.hash = "", this.beforeHash = ""});
 
   @override
   String toString() {
     return 'GitCommit{asciiGraph: $asciiGraph, hash: $hash, abbreviatedHash: $abbreviatedHash, message: $message, date: $date, author: $author}';
   }
+}
+
+class GraphHelper {
+  bool right_to_up = false;
+  bool right_from_down = false;
+  bool left_to_up = false;
+  bool left_from_down = false;
+  bool right_to_right = false;
+  bool left_to_left = false;
+  bool commit = false;
+  bool vertical = false;
+  bool horizontal = false;
+  bool cross = false;
+  bool cross_commit = false;
 }
