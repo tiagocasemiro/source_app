9dba2bdcc8d8d74ad4182046cb9b406e2e11892b
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-11-05 09:53:55 -0300

    rename graph

--- lib/engine/domain/model/git_commit.dart
+++ lib/engine/domain/model/git_commit.dart
@@ -1,22 +1,21 @@
 class GitCommit {
-  String asciiGraph;
   String hash;
   String abbreviatedHash;
   String message;
   String date;
   String author;
   String beforeHash;
-  GraphHelper graphHelper;
+  List<Graph> graph;
 
-  GitCommit({this.asciiGraph = "", this.abbreviatedHash = "", this.author = "", this.message = "", this.date = "", this.hash = "", this.beforeHash = ""});
+  GitCommit({this.graph, this.abbreviatedHash = "", this.author = "", this.message = "", this.date = "", this.hash = "", this.beforeHash = ""});
 
   @override
   String toString() {
-    return 'GitCommit{asciiGraph: $asciiGraph, hash: $hash, abbreviatedHash: $abbreviatedHash, message: $message, date: $date, author: $author}';
+    return 'GitCommit{hash: $hash, abbreviatedHash: $abbreviatedHash, message: $message, date: $date, author: $author}';
   }
 }
 
-class GraphHelper {
+class Graph {
   bool right_to_up = false;
   bool right_from_down = false;
   bool left_to_up = false;
