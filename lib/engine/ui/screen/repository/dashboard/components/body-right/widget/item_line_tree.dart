a63e7929e89fb52daadd2d97d0a1a82fd31b426f
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-11-05 09:54:23 -0300

    ajust to no crash now

--- lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_line_tree.dart
+++ lib/engine/ui/screen/repository/dashboard/components/body-right/widget/item_line_tree.dart
@@ -2,13 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:source_app/engine/domain/model/git_commit.dart';
 import 'package:source_app/engine/ui/source_resources.dart';
 
 class ItemLineTree extends StatelessWidget {
-  final String lineTree;
+  final List<Graph> graph;
   final double singleLine = 30.0;
+  final int index;
 
-  const ItemLineTree(this.lineTree);
+  const ItemLineTree(this.graph, this.index);
 
   //http://bit-booster.com/graph.html
   //https://stackoverflow.com/a/34987899/7249382
@@ -18,10 +20,10 @@ class ItemLineTree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = lineTree != null? lineTree.length * singleLine : singleLine;
+    double width = graph != null? graph.length * singleLine : singleLine;
 
     return Visibility(
-      visible: lineTree != null,
+      visible: graph != null,
       replacement: Container(width: width,),
       child: Container(
         color: SourceColors.grey[2],
@@ -35,15 +37,6 @@ class ItemLineTree extends StatelessWidget {
 
   List<Widget> buildLine() {
     List<Widget> line = List();
-    List<String> patterns = lineTree.split("");
-    patterns.forEach((pattern) {
-      if(pattern != null) {
-        Widget flow = patternToWidget(pattern);
-        if(flow != null) {
-          line.add(patternToWidget(pattern));
-        }
-      }
-    });
 
     if(line.isEmpty)
       line.add(Container());
@@ -56,31 +49,35 @@ class ItemLineTree extends StatelessWidget {
 
     switch(pattern.trim()){
       case "|": {
-        image = "images/flow-vertical.svg";
+        image = "images/flow/pipe.svg";
+        break;
+      }
+      case "/": {
+        image = "images/flow/bar.svg";
+        break;
+      }
+      case "\\": {
+        image = "images/flow/back-slash.svg";
+        break;
+      }
+      case "-": {
+        image = "images/flow/trace.svg";
         break;
       }
       case "*": {
-        image = "images/flow-simple-circle.svg";
+        if(index == 0) {
+          image = "images/flow/start-asterisk.svg";
+        } else {
+          image = "images/flow/asterisk.svg";
+        }
         break;
       }
+
       default: {
-        return Container(width:10, height: singleLine,);
+        return null;
       }
     }
 
-   return  SvgPicture.asset(image, width: 10, height: singleLine);
+   return  SvgPicture.asset(image, height: singleLine, color: SourceColors.blue,);
   }
 }
-
-/*
-images/flow-cross-circle.svg
-
-images/flow-from-left-circle.svg
-
-images/flow-from-right-circle.svg
-images/flow-horizontal.svg
-
-images/flow-to-left.svg
-images/flow-to-right.svg
-
-*/
