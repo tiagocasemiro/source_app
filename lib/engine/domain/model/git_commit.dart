293abd6701fd9f870d345df0af72d93efab4d490
tiagocasemiro <tiagocasemiro@hotmail.com>
2020-11-25 09:24:14 -0300

    rename images

--- lib/engine/domain/model/git_commit.dart
+++ lib/engine/domain/model/git_commit.dart
@@ -17,13 +17,14 @@ class GitCommit {
 
 class Graph {
   String hash;
-  bool right_to_up = false;
-  bool right_from_down = false;
+  bool top_left = false;
+  bool bottom_left = false;
   bool right_to_right = false;
   bool left_to_left = false;
-  bool left_to_up = false;
-  bool left_from_down = false;
+  bool top_right = false;
+  bool bottom_right = false;
   bool commit = false;
+  bool end_commit = false;
   bool start_commit = false;
   bool vertical = false;
   bool horizontal = false;
@@ -33,5 +34,33 @@ class Graph {
   @override
   String toString() {
     return '$hash';
+  }
+
+  bool isEmpty() {
+    return hash == null &&
+      !top_left &&
+      !bottom_left  &&
+      !right_to_right  &&
+      !left_to_left  &&
+      !top_right  &&
+      !bottom_right  &&
+      !commit  &&
+      !start_commit  &&
+      !vertical  &&
+      !horizontal;
+  }
+
+  bool onlyVertical() {
+    return hash != null &&
+      !top_left &&
+      !bottom_left  &&
+      !right_to_right  &&
+      !left_to_left  &&
+      !top_right  &&
+      !bottom_right  &&
+      !commit  &&
+      !start_commit  &&
+      !horizontal &&
+      vertical;
   }
 }
